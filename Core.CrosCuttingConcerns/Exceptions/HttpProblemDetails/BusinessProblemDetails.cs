09fb024591b3f3e0d886c8718d27e6214536edb9
henimex <henimex@gmail.com>
2024-11-19 22:47:14 +0300

    Exception Contrrol Center

---
+++ Core.CrosCuttingConcerns/Exceptions/HttpProblemDetails/BusinessProblemDetails.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrosCuttingConcerns.Exceptions.HttpProblemDetails;
+
+public class BusinessProblemDetails : ProblemDetails
+{
+    public BusinessProblemDetails(string detail)
+    {
+        //RFC Rules
+        Title = "Rule Validation";
+        Detail = detail;
+        Status = StatusCodes.Status400BadRequest;
+        Type = "https://example.com/probs/business";
+        Instance = "/api/unknow_resource/";
+    }
+}
---
+++ Core.CrosCuttingConcerns/Exceptions/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Core.CrosCuttingConcerns.Exceptions.Handlers;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrosCuttingConcerns.Exceptions.Middleware;
+
+public class ExceptionMiddleware 
+{
+    private readonly RequestDelegate _next;
+    private readonly ExceptionHandler _exceptionHandler;
+
+    public ExceptionMiddleware(ExceptionHandler exceptionHandler, RequestDelegate next)
+    {
+        _exceptionHandler = exceptionHandler;
+        _next = next;
+    }
+}
