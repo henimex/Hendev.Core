853d67ccb0c0153a7fc6c455905316cfc6135705
henimex <henimex@gmail.com>
2024-11-20 01:50:39 +0300

    Exception Middleware Bugs Removed

---
+++ Core.CrosCuttingConcerns/Exceptions/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Core.CrosCuttingConcerns.Exceptions.Middleware;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrosCuttingConcerns.Exceptions.Extensions;
+
+public static class ExceptionMiddlewareExtensions
+{
+    public static void ConfigureCustomExceptionMiddleware(this IApplicationBuilder app)
+        => app.UseMiddleware<ExceptionMiddleware>();
+}
--- Core.CrosCuttingConcerns/Exceptions/Middleware/ExceptionMiddleware.cs
+++ Core.CrosCuttingConcerns/Exceptions/Middleware/ExceptionMiddleware.cs
@@ -1,21 +1,35 @@
 ﻿using Core.CrosCuttingConcerns.Exceptions.Handlers;
 using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.CrosCuttingConcerns.Exceptions.Middleware;
 
 public class ExceptionMiddleware 
 {
     private readonly RequestDelegate _next;
-    private readonly ExceptionHandler _exceptionHandler;
+    private readonly HttpExceptionHandler _httpExceptionHandler;
 
-    public ExceptionMiddleware(ExceptionHandler exceptionHandler, RequestDelegate next)
+    public ExceptionMiddleware(RequestDelegate next)
     {
-        _exceptionHandler = exceptionHandler;
         _next = next;
+        _httpExceptionHandler = new HttpExceptionHandler();
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception exception)
+        {
+            await HandleExceptionAsync(context.Response, exception);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpResponse response, Exception exception)
+    {
+        response.ContentType = "application/json";
+        _httpExceptionHandler.Response = response;
+        return _httpExceptionHandler.HandleExceptionAsync(exception);
     }
 }
