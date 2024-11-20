71150d1b6f17d441ce4720586d3de77467218333
henimex <henimex@gmail.com>
2024-11-20 19:20:20 +0300

    Caching Behavior Added.

---
+++ Core.Application/Pipelines/Caching/CachingBehavior.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Core.Application.Pipelines.Caching;
+
+public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>, ICacheRequest
+{
+    private readonly CacheSettings _cacheSettings;
+    private readonly IDistributedCache _cache;
+    private readonly ILogger<CachingBehavior<TRequest, TResponse>> _logger;
+
+    public CachingBehavior(IDistributedCache cache, IConfiguration configuration, ILogger<CachingBehavior<TRequest, TResponse>> logger)
+    {
+        _cacheSettings = configuration.GetSection("CacheSettings").Get<CacheSettings>() ?? throw new InvalidOperationException();
+        _cache = cache;
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (request.BypassCache)
+            return await next();
+
+        TResponse response;
+        byte[]? cachedResponse = await _cache.GetAsync(request.CacheKey, cancellationToken);
+
+        if (cachedResponse != null)
+        {
+            response = JsonSerializer.Deserialize<TResponse>(Encoding.Default.GetString(cachedResponse))!;
+            //TODO::Check Area For ! Mark
+            _logger.LogInformation($"Fetched from Cache -> {request.CacheKey}");
+        }
+        else
+        {
+            response = await GetResponseAndAddToCache(request, next, cancellationToken);
+        }
+
+        return response;
+    }
+
+    private async Task<TResponse> GetResponseAndAddToCache(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        TResponse response = await next();
+
+        TimeSpan expiration = request.SlidingExpiration ?? TimeSpan.FromDays(_cacheSettings.SlidingExpiration);
+        DistributedCacheEntryOptions cacheOptions = new() {SlidingExpiration = expiration};
+
+        byte[] serializeDData = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response));
+
+        await _cache.SetAsync(request.CacheKey, serializeDData, cacheOptions, cancellationToken);
+        _logger.LogInformation($"Added to Cache -> {request.CacheKey}");
+
+        return response;
+    }
+
+    //private async Task addCacheKeyToGroup(TRequest request, TimeSpan slidingExpiration, CancellationToken cancellationToken)
+    //{
+    //    byte[]? cacheGroupCache = await _cache.GetAsync(key: request.CacheGroupKey!, cancellationToken);
+    //    HashSet<string> cacheKeysInGroup;
+    //    if (cacheGroupCache != null)
+    //    {
+    //        cacheKeysInGroup = JsonSerializer.Deserialize<HashSet<string>>(Encoding.Default.GetString(cacheGroupCache))!;
+    //        if (!cacheKeysInGroup.Contains(request.CacheKey))
+    //            cacheKeysInGroup.Add(request.CacheKey);
+    //    }
+    //    else
+    //        cacheKeysInGroup = new HashSet<string>(new[] { request.CacheKey });
+    //    byte[] newCacheGroupCache = JsonSerializer.SerializeToUtf8Bytes(cacheKeysInGroup);
+
+    //    byte[]? cacheGroupCacheSlidingExpirationCache = await _cache.GetAsync(
+    //        key: $"{request.CacheGroupKey}SlidingExpiration",
+    //        cancellationToken
+    //    );
+    //    int? cacheGroupCacheSlidingExpirationValue = null;
+    //    if (cacheGroupCacheSlidingExpirationCache != null)
+    //        cacheGroupCacheSlidingExpirationValue = Convert.ToInt32(Encoding.Default.GetString(cacheGroupCacheSlidingExpirationCache));
+    //    if (cacheGroupCacheSlidingExpirationValue == null || slidingExpiration.TotalSeconds > cacheGroupCacheSlidingExpirationValue)
+    //        cacheGroupCacheSlidingExpirationValue = Convert.ToInt32(slidingExpiration.TotalSeconds);
+    //    byte[] serializeCachedGroupSlidingExpirationData = JsonSerializer.SerializeToUtf8Bytes(cacheGroupCacheSlidingExpirationValue);
+
+    //    DistributedCacheEntryOptions cacheOptions =
+    //        new() { SlidingExpiration = TimeSpan.FromSeconds(Convert.ToDouble(cacheGroupCacheSlidingExpirationValue)) };
+
+    //    await _cache.SetAsync(key: request.CacheGroupKey!, newCacheGroupCache, cacheOptions, cancellationToken);
+    //    _logger.LogInformation($"Added to Cache -> {request.CacheGroupKey}");
+
+    //    await _cache.SetAsync(
+    //        key: $"{request.CacheGroupKey}SlidingExpiration",
+    //        serializeCachedGroupSlidingExpirationData,
+    //        cacheOptions,
+    //        cancellationToken
+    //    );
+    //    _logger.LogInformation($"Added to Cache -> {request.CacheGroupKey}SlidingExpiration");
+    //}
+}
---
+++ Core.Application/Pipelines/Caching/ICacheRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Pipelines.Caching;
+
+
+public interface ICacheRequest
+{
+    //TODO:: Check Area Naming Changed
+    string CacheKey { get; }
+    bool BypassCache { get; }
+    TimeSpan? SlidingExpiration { get; }
+}
