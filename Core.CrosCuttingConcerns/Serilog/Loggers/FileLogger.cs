7e131654c4662719f828889e9c090e125e997a3d
henimex <henimex@gmail.com>
2024-11-21 07:31:29 +0300

    Logging Pipeline

---
+++ Core.CrosCuttingConcerns/Serilog/Loggers/FileLogger.cs
@@ -0,0 +1,30 @@
+﻿using Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+using Core.CrossCuttingConcerns.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace Core.CrossCuttingConcerns.Serilog.Logger;
+
+public class FileLogger:LoggerServiceBase
+{
+    private readonly IConfiguration configuration;
+
+    public FileLogger(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+
+        FileLogConfiguration logConfig = configuration
+            .GetSection("Loggers:Serilog:Config:FileLogConfig")
+            .Get<FileLogConfiguration>() ?? 
+            throw new Exception(SerilogMessages.NullOptionsMessage);
+
+        string logFilePath = string.Format(format: "{0}{1}", arg0: Directory.GetCurrentDirectory() + logConfig.FolderPath, arg1: ".txt");
+
+        Logger = new LoggerConfiguration().WriteTo.File(
+            logFilePath, rollingInterval: RollingInterval.Day,
+            retainedFileCountLimit: null,
+            fileSizeLimitBytes: 5000000,
+            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}"
+        ).CreateLogger();
+    }
+}
