bb7ef9e8dd6061f9563fc813f0129fad5bca88be
henimex <henimex@gmail.com>
2024-11-23 01:54:20 +0300

    Security Section Completed

---
+++ Core.Application/Pipelines/Authorization/AuthorizationBehaviour.cs
@@ -0,0 +1,69 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Types;
+using Core.Security.Constants;
+using Core.Security.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.Application.Pipelines.Authorization;
+
+public class AuthorizationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>, ISecuredRequest
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public AuthorizationBehavior(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        List<string>? userRoleClaims = _httpContextAccessor.HttpContext.User.ClaimRoles();
+
+        if (userRoleClaims == null || !userRoleClaims.Any())
+            throw new AuthorizationException("You are not authenticated.");
+
+        bool isAuthorized = userRoleClaims.Any(userRoleClaim =>
+            userRoleClaim == GeneralOperationClaims.Admin ||
+            request.Roles.Contains(userRoleClaim));
+
+        if (!isAuthorized)
+        {
+            string requiredRolesMessage = string.Join(", ", request.Roles);
+            throw new AuthorizationException($"You are not authorized. Required Claim(s): {requiredRolesMessage}");
+        }
+
+        TResponse response = await next();
+        return response;
+    }
+}
+
+#region SourceCode  
+
+//Code Changed Because of IsNullOrEmpty() and required role info added to UnAuthorized message
+//public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+//{
+//    List<string>? userRoleClaims = _httpContextAccessor.HttpContext.User.ClaimRoles();
+
+//    foreach (var item in request.Roles)
+//    {
+//        Console.WriteLine(item);
+//    }
+
+//    if (userRoleClaims == null)
+//        throw new AuthorizationException("You are not authenticated.");
+
+//    bool isNotMatchedAUserRoleClaimWithRequestRoles = userRoleClaims
+//        .FirstOrDefault(
+//            userRoleClaim => userRoleClaim == GeneralOperationClaims.Admin ||
+//                             request.Roles.Any(role => role == userRoleClaim)).IsNullOrEmpty();
+
+//    if (isNotMatchedAUserRoleClaimWithRequestRoles)
+//        throw new AuthorizationException("You are not authorized.");
+
+//    TResponse response = await next();
+//    return response;
+//}
+
+#endregion
---
+++ Core.Security/Constants/GeneralOperationClaims.cs
@@ -0,0 +1,36 @@
+﻿namespace Core.Security.Constants;
+
+public static class GeneralOperationClaims
+{
+    public const string Admin = "Admin";
+    public const string Mod = "Mod";
+    public const string User = "User";
+    public const string Read = "Read";
+    public const string Write = "Write";
+}
+
+//public static class CommonClaimNames
+//{
+//    public const string ReadSingle = "ReadSingle";
+//    public const string ReadList = "ReadList";
+//    public const string ReadAll = "ReadAll";
+//    public const string Write = "Write";
+//    public const string Update = "Update";
+//    public const string Delete = "Delete";
+//}
+
+//public static class EntityClaimNames
+//{
+//    public const string Brand = "Brand";
+//    public const string Model = "Model";
+//    public const string Transmission = "Transmission";
+//    public const string Fuel = "Fuel";
+//}
+
+//public static class SpecifiedClaims
+//{
+//    public const EntityClaimNames = Brand
+//    public const string Model = "Model";
+//    public const string Transmission = "Transmission";
+//    public const string Fuel = "Fuel";
+//}
